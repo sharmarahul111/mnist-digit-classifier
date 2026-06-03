caf32345be558c7b38a258b78dfc7d7cc1971908
Pramendra Sharma <sharmarahul111.psr@gmail.com>
2026-06-03 22:09:03 +0545

    save models in file

--- utils.py
+++ utils.py
@@ -11,3 +11,15 @@ def accuracy(network, inputs, targets):
     outputs = np.array([network.forward(inp) for inp in inputs])
     predictions = np.argmax(outputs, axis=1)
     return 100 * np.mean(predictions == targets)
+
+# save.py
+import pickle
+
+def save_model(model, filename="model.pkl"):
+    with open(filename, "wb") as f:
+        pickle.dump(model, f)
+
+
+def load_model(filename="model.pkl"):
+    with open(filename, "rb") as f:
+        return pickle.load(f)
