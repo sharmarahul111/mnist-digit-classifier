3aed51eab517f2d7bb969b8c61664cd8f3e0be70
Pramendra Sharma <sharmarahul111.psr@gmail.com>
2026-06-04 05:55:03 +0545

    num_to_probability helper added

--- utils.py
+++ utils.py
@@ -8,18 +8,24 @@ def test_set():
 	return np.loadtxt('data/mnist_test.csv', delimiter=',')
 
 def accuracy(network, inputs, targets):
-    outputs = np.array([network.forward(inp) for inp in inputs])
-    predictions = np.argmax(outputs, axis=1)
-    return 100 * np.mean(predictions == targets)
+	outputs = np.array([network.forward(inp) for inp in inputs])
+	predictions = np.argmax(outputs, axis=1)
+	accur = 100 * np.mean(predictions == targets)
+	return accur
+
+def num_to_probability(num):
+	x = np.zeros(10)
+	x[int(num)] = 0.99
+	return x
 
 # save.py
 import pickle
 
 def save_model(model, filename="model.pkl"):
-    with open(filename, "wb") as f:
-        pickle.dump(model, f)
+	with open(filename, "wb") as f:
+		pickle.dump(model, f)
 
 
 def load_model(filename="model.pkl"):
-    with open(filename, "rb") as f:
-        return pickle.load(f)
+	with open(filename, "rb") as f:
+		return pickle.load(f)
